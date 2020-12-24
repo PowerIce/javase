a4e941f4433a178599e8913a0edf775d75df5a53
hailong.zhao <1qaz2wsx>
2020-12-24 15:58:59 +0800

    [A] first commit

---
+++ javaSe/src/bing/Main.java
@@ -0,0 +1,12 @@
+package bing;
+
+public class Main {
+    public static void main (String[] args) {
+        int a = 72;
+        int b = 105;
+        int c = 65281;
+        // FIXME:
+        String s = "" + (char)a + (char)b + (char)c;
+        System.out.println(s);
+    }
+}
